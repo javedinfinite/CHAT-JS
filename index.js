4fa7ad4fae13db1c8a3a412ec7c54143985519a5
javedinfinite <javedaktar73@gmail.com>
2018-01-05 21:20:12 +0530

    First version of application moved into github

---
+++ index.js
@@ -0,0 +1,57 @@
+var http = require('http');
+var fs = require('fs');
+var  server = require('ws').Server;
+var s  = new server({port:5001});
+
+ 
+ var temp = http.createServer(function (req, resp) {
+ 
+    fs.readFile('index.html', function (error, pgResp) {
+        if (error) {
+            resp.writeHead(404);
+            resp.write('Contents you are looking are Not Found');
+        } else {
+            resp.writeHead(200, { 'Content-Type': 'text/html' });
+            resp.end(pgResp);
+		//pgResp.sendFile(__dirname + '/index.html');
+        }
+    });
+});
+temp.listen(5002);
+console.log('server is running.....');
+// When a connection is established
+s.on('connection', function(ws) {
+	console.log('one more client is added');
+	// When data is received
+	ws.on('message',function(message){
+
+		message = JSON.parse(message);//since message is in json format hence we
+		if(message.type=="name"){
+			ws.personName = message.data;
+			return;
+		}
+		console.log("Received from "+ws.personName+",message="+message.data);
+
+		//broadcast received message to every client online by iterating over all the connected client
+		s.clients.forEach(function e(client){
+			if(client!= ws)//if client is not the current socket(i.e. message sender client)
+ 				client.send(JSON.stringify({
+					name: ws.personName,
+					data:	message.data			
+				}));		 
+		
+		});
+
+
+			
+		//ws.send("message received by the server is :"+ message);
+			
+	});
+	// when socket is closed
+	ws.on('close',function(){
+		console.log("One client is disconnected");	
+	});
+});
+
+
+ 
